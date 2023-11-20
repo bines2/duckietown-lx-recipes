11bc2f5b98fcc7f8b07879750e614e826fed9f69
bines2 <145353613+bines2@users.noreply.github.com>
2023-11-20 13:41:49 +0100

    Update visual_lane_servoing_node.py

--- visual-lane-servoing/packages/visual_lane_servoing/src/visual_lane_servoing_node.py
+++ visual-lane-servoing/packages/visual_lane_servoing/src/visual_lane_servoing_node.py
@@ -38,7 +38,7 @@ class LaneServoingNode(DTROS):
         self.veh = rospy.get_namespace().strip("/")
 
         # The following are used for the Braitenberg exercise
-        self.v_0 = 0.15  # Forward velocity command
+        self.v_0 = 0.2  # Forward velocity command
 
         # The following are used for scaling
         self.steer_max = -1
